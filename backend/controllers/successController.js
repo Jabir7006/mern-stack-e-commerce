ad696dc8b3fc4cbd5f92b22df270a2334edb1b9d
Jabir7006 <www.roman1249@gmail.com>
2023-10-26 12:55:18 +0600

    first commit

--- backend/controllers/successController.js
+++
@@ -1,9 +0,0 @@
-successResponse = (res, { statusCode = 200, message = "Success", payload = {} }) => {
-  return res.status(statusCode).send({
-    success: true,
-    message,
-    payload,
-  });
-};
-
-module.exports = successResponse;
