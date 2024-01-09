ff57f5e344e71daba04a0a825facd1b8f061d30b
Jabir7006 <www.roman1249@gmail.com>
2024-01-09 16:24:02 +0600

    updated

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://mern-ecommerce-app-backend-jkkk.onrender.com";
 
 export const handleRegister = async (userdata) => {
   const response = await axios.post(`${baseUrl}/api/users/register`, userdata);
